733db2859b6cb9f774a8ee95225e39d76867e9f6
Thygo11 <9025896@student.zadkine.nl>
2025-09-15 13:14:34 +0200

    Opdracht 1.2

--- 1-js-methods/level-1-basis-methods/1.2-map-oefening/app.js
+++ 1-js-methods/level-1-basis-methods/1.2-map-oefening/app.js
@@ -1,11 +1,12 @@
 // Opdracht 1.2: Map Oefening
 console.log('🚀 Opdracht 1.2: Map Oefening');
 
-const namen = ['sanne', 'johan', 'sjoerd'];
+const namen = ['jan', 'piet', 'klaas'];
 console.log('Originele namen:', namen);
 
 // TODO: Zet alle namen om naar hoofdletter
-const hoofdletterNamen = /* jouw code hier */
+const hoofdletterNamen =  ['Jan, Piet, Klaas']
+map = hoofdletterNamen
 
 console.log('Namen met hoofdletter:', hoofdletterNamen);
-console.log('Verwacht resultaat: [\'Sanne\', \'Johan\', \'Sjoerd\']');
+console.log('Verwacht resultaat: [\'Jan\', \'Piet\', \'Klaas\']');
