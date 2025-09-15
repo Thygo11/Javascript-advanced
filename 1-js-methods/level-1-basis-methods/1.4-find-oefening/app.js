a4ee1236bc245ebd9a45feeb05f742467e55baa9
Thygo11 <9025896@student.zadkine.nl>
2025-09-15 14:18:24 +0200

    Opdracht 1.4

--- 1-js-methods/level-1-basis-methods/1.4-find-oefening/app.js
+++ 1-js-methods/level-1-basis-methods/1.4-find-oefening/app.js
@@ -1,15 +1,9 @@
-// Opdracht 1.4: Find Oefening
-console.log('🚀 Opdracht 1.4: Find Oefening');
+console.log('🚀 Opdracht: Eerste getal groter dan 50');
 
 const getallen = [12, 45, 67, 23, 89, 34];
 console.log('Getallen array:', getallen);
 
 // TODO: Zoek het eerste getal groter dan 50
-const gevondenGetal = /* jouw code hier */
+const gevondenGetal = getallen.find(getal => getal > 50);
 
 console.log('Eerste getal > 50:', gevondenGetal);
-
-// TODO: Zoek de index van het eerste getal groter dan 50
-const gevondenIndex = /* jouw code hier */
-
-console.log('Index van eerste getal > 50:', gevondenIndex);
