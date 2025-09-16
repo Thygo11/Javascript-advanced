790e958e7f6c818358436954916adecfbeb61f00
Thygo11 <9025896@student.zadkine.nl>
2025-09-16 11:36:14 +0200

    Opdracht 2.3

--- 1-js-methods/level-2-combinaties/2.3-find-en-findindex/app.js
+++ 1-js-methods/level-2-combinaties/2.3-find-en-findindex/app.js
@@ -1,24 +1,24 @@
 // Opdracht 2.3: Find en FindIndex
 console.log('🚀 Opdracht 2.3: Find en FindIndex');
 
-const temperaturen = [18, 22, 25, 19, 28, 24, 21];
+const temperaturen = [18, 22, 25, 17, 28, 24, 21];
 const kleuren = ['rood', 'blauw', 'groen', 'geel', 'paars', 'oranje'];
 
 console.log('Temperaturen:', temperaturen);
 console.log('Kleuren:', kleuren);
 
 // TODO: Zoek de eerste temperatuur boven 25 graden
-const warmeTemperatuur = /* jouw code hier */
+const warmeTemperatuur = temperaturen.find(tempratuur => tempratuur > 25);
 console.log('Eerste warme temperatuur:', warmeTemperatuur);
 
 // TODO: Zoek de index van de kleur 'groen'
-const groenIndex = /* jouw code hier */
+const groenIndex = kleuren.findIndex(kleur => kleur === "groen")
 console.log('Index van groen:', groenIndex);
 
 // TODO: Zoek de eerste temperatuur onder 20 graden
-const koudeTemperatuur = /* jouw code hier */
+const koudeTemperatuur = temperaturen.find(tempratuur => tempratuur < 20);
 console.log('Eerste koude temperatuur:', koudeTemperatuur);
 
 // TODO: Zoek de index van de eerste temperatuur boven 23 graden
-const warmeIndex = /* jouw code hier */
+const warmeIndex = temperaturen.findIndex(temprature=> temprature > 23)
 console.log('Index van eerste warme temperatuur:', warmeIndex);
