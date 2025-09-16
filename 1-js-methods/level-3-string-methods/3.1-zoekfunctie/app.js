9b1a96d14af89be7d17616ad92882d327b7c6465
Thygo11 <9025896@student.zadkine.nl>
2025-09-16 13:00:06 +0200

    Opdracht 3.1

--- 1-js-methods/level-3-string-methods/3.1-zoekfunctie/app.js
+++ 1-js-methods/level-3-string-methods/3.1-zoekfunctie/app.js
@@ -6,8 +6,12 @@ console.log('Alle films:', films);
 
 function zoekFilms(films, zoekterm) {
     // TODO: Return films die de zoekterm bevatten
-    return /* jouw code hier */;
+    return films.filter(film =>
+        film.toLowerCase().includes(zoekterm.toLowerCase())
+    );
 }
+
+zoekFilms(films, "avatar");
 
 // Test de functie
 console.log('Zoek "the":', zoekFilms(films, 'the'));
