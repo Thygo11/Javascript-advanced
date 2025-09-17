f0bdec2809807b082e014cc3f4242eb65f84cd7b
Thygo11 <9025896@student.zadkine.nl>
2025-09-17 10:24:46 +0200

    Opdracht 3.4

--- 1-js-methods/level-3-string-methods/3.4-email-validatie/app.js
+++ 1-js-methods/level-3-string-methods/3.4-email-validatie/app.js
@@ -1,25 +1,37 @@
-// Opdracht 3.4: Email Validatie
 console.log('🚀 Opdracht 3.4: Email Validatie');
 
 function valideerEmail(email) {
-    // TODO: Check of email geldig is
-    // Regels: moet @ bevatten, geen spaties, eindigt op .com/.nl/.org
+    const schoonEmail = email.trim();
+
     
-    const schoonEmail = /* jouw code hier - trim en lowercase */
+    if (!schoonEmail.includes('@')) {
+        return false;
+    }
+
+    if (schoonEmail.includes(' ')) {
+        return false;
+    }
+
     
-    // Check @ aanwezig
-    if () return false;
-    
-    // Check geen spaties
-    if () return false;
-    
-    // Check geldig einde
     const geldigeEindes = ['.com', '.nl', '.org'];
-    return /* jouw code hier - check of email eindigt op een van deze */;
+    const heeftGeldigEinde = geldigeEindes.some(ext => schoonEmail.endsWith(ext));
+
+    if (!heeftGeldigEinde) {
+        return false;
+    }
+
+    return true;
 }
 
-// Test de functie
-const testEmails = ['jan@test.com', 'marie@voorbeeld.nl', 'fout@test.be', 'fout email@test.com'];
+
+const testEmails = [
+    'jan@test.com',
+    'marie@voorbeeld.nl',
+    'fout@test.be',
+    'fout email@test.com'
+];
+
 testEmails.forEach(email => {
     console.log(`${email}: ${valideerEmail(email)}`);
 });
+       
