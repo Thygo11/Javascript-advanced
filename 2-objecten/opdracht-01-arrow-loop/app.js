0b2ac14c2e5c14acb11b9846b5013a9e51f4200b
Thygo11 <9025896@student.zadkine.nl>
2025-09-30 10:53:41 +0200

    Arrow-loop

--- 2-objecten/opdracht-01-arrow-loop/app.js
+++ 2-objecten/opdracht-01-arrow-loop/app.js
@@ -1,8 +1,18 @@
-// Je gaat even oefenen met het gebruiken van een arrow function en de for/of loop. 
+const films = ["Spiderman", "Thor", "Deadpool", "Avengers"];  
 
-// Maak een array genaamd 'marvels' en plaats hier 4 Marvel films in.
+const calculate = (a, b) => {
+  console.log(a * b);
+};
 
-// Maak een arrow function genaamd 'addMarvel'
-// In je functie voeg je een nieuwe Marvel film toe zonder dit zelf in de bovenstaande array toe te voegen
-// Roep de functie op (het klopt dat je nog niets ziet)
-// Loop met een for/of loop door de array en toon alle items in de array in je console
+function voegMarvelFilmToe(filmNaam) {
+  films.unshift(filmNaam);             
+  console.log(`Film toegevoegd: ${filmNaam}`);
+}
+
+
+voegMarvelFilmToe("Black Panther");
+
+
+for (let film of films) {
+  console.log(film);
+}
