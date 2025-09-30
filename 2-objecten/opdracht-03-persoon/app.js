8073fd3f6f3fe40fade6bb495d2858202358c5c9
Thygo11 <9025896@student.zadkine.nl>
2025-09-30 11:39:31 +0200

    Persoon object opdracht 3

--- 2-objecten/opdracht-03-persoon/app.js
+++ 2-objecten/opdracht-03-persoon/app.js
@@ -1,25 +1,45 @@
-// Stap 1
+const person = {
+  name: "John",
+  age: 35,
+  dateOfBirth: "1989-04-15",
+  gender: "male",
+  isAlive: true,
+  hobbies: ["reading", "cycling", "cooking", "Running"],
 
-// Maak een persoon object waar je allemaal eigenschappen van een persoon in kunt opslaan en tonen op het scherm.
 
-// Het object moet de volgende properties bevatten:
+  changeName: function(newName) {
+    this.name = newName;
+    displayPerson();
+  },
 
-//     Naam: de naam van de persoon (string)
-//     Leeftijd: de leeftijd van de persoon (number)
-//     Geboortedatum: de geboortedatum van de persoon (string)
-//     Geslacht: het geslacht van de persoon (string)
-//     In leven: geef aan of de persoon nog leeft (boolean)
-//     Hobby’s: 4 hobby’s van de persoon (array)
+  changeIsAlive: function(status) {
+    this.isAlive = status;
+    displayPerson();
+  },
 
-// Toon elke property op het scherm door het in een p-tag te plaatsen. Hiervoor gebruik je de div-tag van de vorige opdracht.
-// Stap 2:
+  addHobby: function(newHobby) {
+    this.hobbies.push(newHobby);
+    displayPerson();
+  }
+};
 
-// Het object moet ook de volgende methods bevatten:
 
-//     Een method (functie) die de naam moet veranderen. De naam moet je via de parameters kunnen meegeven.
-//     Een method (functie) die het ‘in leven’ property kan veranderen.
-//     Een method (functie) die een extra hobby toevoegt aan de array. Een hobby moet je via de parameters kunnen meegeven.
+function displayPerson() {
+  const container = document.querySelector(".personInfo");
+  container.innerHTML = `
+    <p>Name: ${person.name}</p>
+    <p>Age ${person.age}</:>
+    <p>Date of Birth: ${person.dateOfBirth}</p>
+    <p>Gender: ${person.gender}</p>
+    <p>Is Alive:${person.isAlive}</p>
+    <p>Hobbies: ${person.hobbies.join(", ")}</p>
+  `;
+}
 
-// Toon de wijzigingen op het scherm.
 
-// Gebruik geen arrow function in een object!
+displayPerson();
+
+
+person.changeName("Emily Johnson");
+person.changeIsAlive(false);
+person.addHobby("painting");
